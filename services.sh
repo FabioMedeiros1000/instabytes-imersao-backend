01bde8c5b5352858e950666661d6cce34bc40022
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-11-23 20:54:16 -0300

    :bug: fix: Adding config in services.h

--- services.sh
+++ services.sh
@@ -1,3 +1,5 @@
+gcloud config set project gen-lang-client-0786601048
+
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
