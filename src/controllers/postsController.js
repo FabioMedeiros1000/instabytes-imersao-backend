61b3b76d33d5a9ded86b4182eb0d93fc0e033130
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-11-23 22:05:58 -0300

    :bug: fix: fixing png image problem

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,7 @@
 import { getTodosPosts, criarPost, atualizarPost } from "../models/postsModel.js"
 import fs from 'fs'
 import gerarDescricaoComGemini from "../services/geminiService.js"
+import path from "path"
 
 export async function listarPosts(req, res) { 
     const posts = await getTodosPosts()
@@ -27,7 +28,9 @@ export async function uploadImagem(req, res) {
 
     try {
         const postCriado = await criarPost(novoPost);
-        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        const novaExtensao = path.extname(req.file.originalname);
+
+        const imagemAtualizada = `uploads/${postCriado.insertedId}${novaExtensao}`;
         fs.renameSync(req.file.path, imagemAtualizada);
         res.status(200).json(postCriado);
     } catch (erro) {
@@ -38,6 +41,7 @@ export async function uploadImagem(req, res) {
 
 export async function atualizarNovoPost(req, res) {
     const id = req.params.id;
+
     const urlImagem = `http://localhost:3000/${id}.png`
     try {
         const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
